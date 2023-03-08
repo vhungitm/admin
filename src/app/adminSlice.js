df22aacd323a407bebe294cbf59121cac707cf47
vhungitm <vhungitm@gmail.com>
2023-03-08 23:54:49 +0700

    Update layout

--- src/app/adminSlice.js
+++ src/app/adminSlice.js
@@ -1,65 +1,67 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-	sidebar: {
-		show: JSON.parse(localStorage.getItem('showSidebar')),
-		menu: [
-			{
-				id: 0,
-				href: '/home',
-				icon: 'menu-home.png',
-				title: 'Home'
-			},
-			{
-				id: 1,
-				href: '/technologies',
-				icon: 'menu-news.png',
-				title: 'Technologies',
-				subTitle: 'Technologies',
-				items: [
-					{
-						id: 0,
-						href: '/technologies/iot',
-						title: 'IoT'
-					},
-					{
-						id: 1,
-						href: '/technologies/5g',
-						title: '5G'
-					}
-				]
-			},
-			{
-				id: 2,
-				href: '/news',
-				icon: 'menu-news.png',
-				title: 'News',
-				subTitle: 'News',
-				items: [
-					{
-						id: 0,
-						title: 'Sub'
-					},
-					{
-						id: 1,
-						title: 'Sub 2'
-					}
-				]
-			}
-		]
-	}
+  sidebar: {
+    show: JSON.parse(localStorage.getItem('showSidebar')),
+    menu: [
+      {
+        id: 0,
+        href: '/home',
+        icon: 'menu-home.png',
+        title: 'Home'
+      },
+      {
+        id: 1,
+        href: '/technologies',
+        icon: 'menu-news.png',
+        title: 'Technologies',
+        subTitle: 'Technologies',
+        items: [
+          {
+            id: 0,
+            href: '/technologies/iot',
+            title: 'IoT'
+          },
+          {
+            id: 1,
+            href: '/technologies/5g',
+            title: '5G'
+          }
+        ]
+      },
+      {
+        id: 2,
+        href: '/news',
+        icon: 'menu-news.png',
+        title: 'News',
+        subTitle: 'News',
+        items: [
+          {
+            id: 0,
+            href: '/news/create',
+            title: 'Create'
+          },
+          {
+            id: 1,
+            href: '/news/update',
+            title: 'Update'
+          }
+        ]
+      }
+    ]
+  }
 };
 
 const adminSlice = createSlice({
-	name: 'admin',
-	initialState,
-	reducers: {
-		setShowSidebar(state, action) {
-			state.sidebar.show = action.payload;
-			localStorage.setItem('showSidebar', JSON.stringify(action.payload));
-			return state;
-		}
-	}
+  name: 'admin',
+  initialState,
+  reducers: {
+    setShowSidebar(state, action) {
+      state.sidebar.show = action.payload;
+      localStorage.setItem('showSidebar', JSON.stringify(action.payload));
+      return state;
+    }
+  }
 });
 
 export const selectAdmin = state => state.admin;
--- src/components/Layout/AdminLayout/Sidebar.jsx
+++ src/components/Layout/AdminLayout/Sidebar.jsx
@@ -6,75 +6,79 @@ import { Link, useNavigate } from 'react-router-dom';
 import './Sidebar.scss';
 
 const Sidebar = () => {
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
-	const { sidebar } = useAppSelector(selectAdmin);
-	const [menuShowId, setMenuShowId] = useState([]);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { sidebar } = useAppSelector(selectAdmin);
+  const [menuShowId, setMenuShowId] = useState([]);
 
-	const onClickMenuItem = menuItem => {
-		// Have sub menu => show sub menu
-		// Else navigate to link
-		if (!menuItem.items) navigate(menuItem.href);
-		else {
-			if (menuShowId === menuItem.id) setMenuShowId(null);
-			else setMenuShowId(menuItem.id);
-		}
-	};
+  const onClickMenuItem = menuItem => {
+    // Have sub menu => show sub menu
+    // Else navigate to link
+    if (!menuItem.items) {
+      navigate(menuItem.href);
+      setMenuShowId(null);
+    } else {
+      if (menuShowId === menuItem.id) setMenuShowId(null);
+      else setMenuShowId(menuItem.id);
+    }
+  };
 
-	const onShow = () => {
-		dispatch(adminActions.setShowSidebar(!sidebar.show));
-	};
+  const onShow = () => {
+    dispatch(adminActions.setShowSidebar(!sidebar.show));
+  };
 
-	return (
-		<div className="sidebar">
-			<div className="sidebar-header">
-				<div className="sidebar-button" onClick={onShow}>
-					<div className="sidebar-button-line line-1" />
-					<div className="sidebar-button-line line-2" />
-					<div className="sidebar-button-line line-3" />
-				</div>
-			</div>
-			<div className="sidebar-menu">
-				{sidebar.menu?.map(item => {
-					const currPath = window.location.pathname;
-					const isActive = currPath.startsWith(item.href);
-					const isShowSubMenu = menuShowId === item.id;
+  return (
+    <div className="sidebar">
+      <div className="sidebar-header">
+        <div className="sidebar-button" onClick={onShow}>
+          <div className="sidebar-button-line line-1" />
+          <div className="sidebar-button-line line-2" />
+          <div className="sidebar-button-line line-3" />
+        </div>
+      </div>
+      <div className="sidebar-menu">
+        {sidebar.menu?.map(item => {
+          const currPath = window.location.pathname;
+          const isActive = currPath.startsWith(item.href);
+          const isShowSubMenu = menuShowId === item.id;
 
-					let menuItemClassName = 'sidebar-menu-item';
-					if (isActive) menuItemClassName += ' is-active';
-					if (isShowSubMenu) menuItemClassName += ' is-show-sub-menu';
+          let menuItemClassName = 'sidebar-menu-item';
+          if (isActive) menuItemClassName += ' is-active';
+          if (isShowSubMenu) menuItemClassName += ' is-show-sub-menu';
 
-					return (
-						<div key={item.id} className={menuItemClassName} onClick={() => onClickMenuItem(item)}>
-							<div className="sidebar-menu-item-content">
-								<img
-									src={'/media/layout/admin/' + item.icon}
-									className="sidebar-menu-item-icon"
-									alt={item.title}
-								/>
-								<div className="sidebar-menu-item-title">{item.title}</div>
-							</div>
+          return (
+            <div key={item.id} className={menuItemClassName} onClick={() => onClickMenuItem(item)}>
+              <div className="sidebar-menu-item-content">
+                <img src={'/media/layout/admin/' + item.icon} className="sidebar-menu-item-icon" alt={item.title} />
+                <div className="sidebar-menu-item-title">{item.title}</div>
+              </div>
 
-							{item.items && (
-								<div className="sidebar-sub-menu">
-									{item.href && (
-										<Link to={item.href} className="sidebar-sub-menu-title">
-											{item.subTitle}
-										</Link>
-									)}
-									{item.items.map(subItem => (
-										<Link to={subItem.href} key={subItem.id} className={'sidebar-sub-menu-item'}>
-											{subItem.title}
-										</Link>
-									))}
-								</div>
-							)}
-						</div>
-					);
-				})}
-			</div>
-		</div>
-	);
+              {item.items && (
+                <div className="sidebar-sub-menu">
+                  {item.href && (
+                    <Link to={item.href} className="sidebar-sub-menu-title">
+                      {item.subTitle}
+                    </Link>
+                  )}
+                  {item.items.map(subItem => {
+                    const subItemIsActive = currPath.startsWith(subItem.href);
+                    let subItemClassName = 'sidebar-sub-menu-item';
+                    if (subItemIsActive) subItemClassName += ' is-active';
+
+                    return (
+                      <Link to={subItem.href} key={subItem.id} className={subItemClassName}>
+                        {subItem.title}
+                      </Link>
+                    );
+                  })}
+                </div>
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
