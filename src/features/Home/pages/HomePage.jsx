2dd895d5214ea6a92d755bfeeac42c1ebc2e50b1
Hung Tran Van <tvhung1>
2023-03-09 11:11:59 +0700

    update min-height

--- src/features/Home/pages/HomePage.jsx
+++ src/features/Home/pages/HomePage.jsx
@@ -1,4 +1,6 @@
-import { useEffect } from 'react';
+import { ButtonIconSplit } from 'components/ITM';
+import { useEffect, useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
@@ -8,10 +10,28 @@ const HomePage = () => {
 		if (window.location.pathname === '/') navigate('/home');
 	}, [navigate]);
 
+	// Modal
+	const [showModal, setShowModal] = useState(true);
+	const handleCloseModal = () => setShowModal(false);
+
 	return (
 		<div className="home">
 			<div className="page-header">
 				<div className="page-header-title">Home</div>
+				<Modal centered show={showModal} onHide={handleCloseModal}>
+					<Modal.Header closeButton>
+						<Modal.Title>Modal heading</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+					<Modal.Footer>
+						<Button variant="outline-danger" onClick={handleCloseModal}>
+							Cancel
+						</Button>
+						<Button variant="outline-primary" onClick={handleCloseModal}>
+							Save data
+						</Button>
+					</Modal.Footer>
+				</Modal>
 			</div>
 		</div>
 	);
